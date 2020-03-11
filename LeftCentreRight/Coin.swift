79274cc9c604195d706d860b51eb488032a0d8d0
Liam Davidson <ldavidson@lcs.on.ca>
2020-03-11 18:46:06 -0400

    Created the file for the Coin enumeration and wrote out the cases with their monetary value. #1

---
+++ LeftCentreRight/Coin.swift
@@ -0,0 +1,15 @@
+//
+//  Coin.swift
+//  LeftCentreRight
+//
+//  Created by Davidson, Liam on 2020-03-11.
+//  Copyright © 2020 Davidson, Liam. All rights reserved.
+//
+
+import Foundation
+
+enum Coin: Double {
+    
+    case toonie = 2.00, loonie = 1.00, quarter = 0.25, dime = 0.10, nickel = 0.05
+    
+}

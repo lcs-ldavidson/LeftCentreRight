7b917127ab8e2af0e41714e9c46f2e1cb29015c1
Liam Davidson <ldavidson@lcs.on.ca>
2020-03-11 18:37:19 -0400

    Initial Commit

---
+++ LeftCentreRight/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LeftCentreRight
+//
+//  Created by Davidson, Liam on 2020-03-11.
+//  Copyright © 2020 Davidson, Liam. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+

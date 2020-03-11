a5b8b8ac34c22e6b8b85377ec602100223d84196
Liam Davidson <ldavidson@lcs.on.ca>
2020-03-11 18:55:43 -0400

    created static variables for the least and most valuable coins in the Coin enumeration. #1

--- LeftCentreRight/Coin.swift
+++ LeftCentreRight/Coin.swift
@@ -8,8 +8,29 @@
 
 import Foundation
 
+//Coin enumeration
 enum Coin: Double {
     
+    //all the possible coins with values as rawValues
     case toonie = 2.00, loonie = 1.00, quarter = 0.25, dime = 0.10, nickel = 0.05
+    
+    //computed value for the monetary value of each coin to make things more readable
+    var value : Double {
+        switch self {
+        default:
+            return self.rawValue
+        }
+    }
+    
+    //static variable for lowest value coin
+    static var leastValuableCoin : Coin {
+        return .nickel
+    }
+    
+    //static variable for most valuable coin
+    static var mostValuableCoin : Coin {
+        return .toonie
+    }
+
     
 }

347e97f5ce94034e6a2c51448c70e6d5302d88f2
Liam Davidson <ldavidson@lcs.on.ca>
2020-03-11 20:27:45 -0400

    Created computed values for total wealth and full description in the stash structure. #2

--- LeftCentreRight/Stash.swift
+++ LeftCentreRight/Stash.swift
@@ -8,6 +8,30 @@
 
 import Foundation
 
+//structure for a collection of coins
 struct Stash {
+    
+    //the array of coins that is contained in the stash
     var collectionOfCoins : [Coin]
+    
+    //returns the total value of all coins in the stash
+    var totalMonetaryValue : Double {
+        var total = 0.0
+        for moneyValue in collectionOfCoins {
+            total += moneyValue.value
+        }
+        return total
+    }
+    
+    //returns a description of each coin in the stash
+    var stashDescription : String {
+        var description = ""
+        for coin in collectionOfCoins {
+            description += coin.description
+        }
+        return description
+    }
+    
+    
+    
 }

d2e0805aefdfc42350be58d6eb689d06880c36b4
Liam Davidson <ldavidson@lcs.on.ca>
2020-04-10 12:25:24 -0400

    Added a function to players that will return the number of dice that need to be rolled each turn. #8

--- LeftCentreRight/FriendGroup.swift
+++ LeftCentreRight/FriendGroup.swift
@@ -89,4 +89,6 @@ struct FriendGroup {
         return playerToTheLeft
     }
     
+    
+    
 }
--- LeftCentreRight/Player.swift
+++ LeftCentreRight/Player.swift
@@ -22,7 +22,19 @@ class Player {
         self.name = name
     }
     
-    
+    func diceToRoll() -> Int {
+        
+        var number : Int = 0
+        
+        if field.collectionOfCoins.count <= 0 {
+            number = 0
+        } else if field.collectionOfCoins.count == 1 || field.collectionOfCoins.count == 2 || field.collectionOfCoins.count == 3 {
+            number = field.collectionOfCoins.count
+        } else if field.collectionOfCoins.count > 3 {
+            number = 3
+        }
+        return number
+    }
     
 }
 
